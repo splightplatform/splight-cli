f4cd3ac3442b7552e95fa463cf8a50b96b49f804
Mariano Piatti <mariano.piatti@splight-ae.com>
2022-04-06 09:55:07 -0300

    Add verifications

--- cli/hub/component.py
+++ cli/hub/component.py
@@ -112,6 +112,12 @@ class Component:
                 raise Exception(f"Component parameter name must be a string")
             if not isinstance(parameter["required"], bool):
                 raise Exception(f"Component parameter required must be a boolean")
+            if [key not in ["name", "type", "required", "value"] for key in parameter.keys()]: 
+                raise Exception(f"Component parameter must have only these keys: name, type, required, value")
+            if "value" in parameter:
+                if type(parameter["value"]) != self.VALID_PARAMETER_VALUES[parameter["type"]]:
+                    raise Exception(f"Component parameter value must be of type {parameter['type']}")
+
         return parameters
 
     def _get_component_zip(self):
