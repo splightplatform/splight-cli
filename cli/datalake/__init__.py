4a6e706edc673e0cdc054c94d938a8af07fdebb7
Matias Silva <matias.silva@splight-ae.com>
2022-06-23 13:17:18 -0300

    Collection default for load/dump

--- cli/datalake/__init__.py
+++ cli/datalake/__init__.py
@@ -10,13 +10,13 @@ logger = logging.getLogger()
 logger.setLevel(logging.WARNING)
 
 @datalake_cli.command()
-@click.argument("collection", nargs=1, type=str)
+@click.option("--collection", '-c', nargs=1, type=str, help="Collection where to load data")
 @click.option("--path", '-p', nargs=1, type=str, help="Path to csv file to be loaded")
 @click.option('--namespace', '-n', help="Namespace of the resource")
 @click.option('--remote', '-r', is_flag=True, help="Load to remote datalake")
 @click.option('--example', '-e', is_flag=True, help="Dump template data")
 @pass_context
-def load(context: Context, collection: str, path: str=None, namespace: str=None, example: bool=None, remote: bool=None) -> None:
+def load(context: Context, collection: str=None, path: str=None, namespace: str=None, example: bool=None, remote: bool=None) -> None:
     """
     Load data into Splight.\n
     Args:\n
@@ -34,22 +34,26 @@ def load(context: Context, collection: str, path: str=None, namespace: str=None,
         if not namespace:
             namespace = user_handler.user_namespace
 
+        if not collection:
+            collection = 'default'
+
         datalake = Datalake(context, namespace)
         datalake.load(collection, path, example, remote)
 
     except Exception as e:
+        logger.exception(e)
         click.echo(f"Error loading data: {str(e)}")
         return
 
 @datalake_cli.command()
-@click.argument("collection", nargs=1, type=str, required=False)
+@click.option("--collection", '-c', nargs=1, type=str, help="Collection where to dump data")
 @click.option('--filter', '-f', multiple=True, default=[], help="Filters (should be in the form of key=value)")
 @click.option("--path", '-p', nargs=1, type=str, default='.')
 @click.option('--namespace', '-n', help="Namespace of the resource")
 @click.option('--remote', '-r', is_flag=True, help="Dump from remote datalake")
 @click.option('--example', '-e', is_flag=True, help="Dump template data")
 @pass_context
-def dump(context: Context, collection: str, path: str, filter: list, namespace: str=None, remote: bool=None, example: bool = False) -> None:
+def dump(context: Context, path: str, filter: list, namespace: str=None, remote: bool=None, example: bool = False) -> None:
     """
     Dump data from Splight.\n
     Args:\n
@@ -57,18 +61,16 @@ def dump(context: Context, collection: str, path: str, filter: list, namespace: 
         path: Path to csv where dump data will be stored.\n
     """
     try:
-        if not example:
-            if collection is None:
-                raise Exception("missing argument COLLECTION")
         user_handler = UserHandler(context)
-        if example and namespace:
-            raise Exception("Cannot specify namespace when dumping example data")
         if not namespace:
             namespace = user_handler.user_namespace
+        if not collection:
+            collection = 'default'
         datalake = Datalake(context, namespace)
         datalake.dump(collection, path, filter, remote, example)
 
     except Exception as e:
+        logger.exception(e)
         click.secho(f"Error dumping data: {str(e)}", fg="red")
         return
 
