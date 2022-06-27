7b5a58a3193a787b374af5079f56343acaa827a5
Ignacio Fernandez <ignacio.fernandez@mi.unc.edu.ar>
2022-06-27 19:00:20 -0300

    upgraded version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ os.system("cat ~/.pypirc")
 
 setup(
     name='splight-cli',
-    version='0.1.3',
+    version='0.1.4',
     author='Splight',
     author_email='factory@splight-ae.com',
     description='Splight developer CLI tool. Splight.',
