5abf6cb7bc8f5b854426a4bf4f0ed68b8430d8ac
marianogaragiola <marianogaragiola@gmail.com>
2022-08-25 10:38:39 -0300

    add creation of startup file

--- cli/component/component.py
+++ cli/component/component.py
@@ -1,15 +1,45 @@
-import os
-import sys
 import importlib
-import subprocess
+import json
 import logging
-from pydantic import BaseModel, validator
+import os
+import subprocess
+import sys
+from pathlib import Path
 from tempfile import NamedTemporaryFile
-from typing import Type, List, Union
-from cli.component.handler import ComponentHandler, UserHandler
-from cli.utils import *
-from cli.constants import *
+from typing import Dict, List, Type, Union
+
+import click
+from jinja2 import Template
+from pydantic import BaseModel, validator
 from splight_lib.component import AbstractComponent
+
+from cli.component.handler import ComponentHandler, UserHandler
+
+# from cli.constants import *
+from cli.constants import (
+    COMPONENT_FILE,
+    DEFAULT_EXTERNAL_ID,
+    DEFAULT_NAMESPACE,
+    INIT_FILE,
+    MAIN_CLASS_NAME,
+    PICTURE_FILE,
+    REQUIRED_FILES,
+    SPEC_FILE,
+    VALID_PARAMETER_VALUES,
+    VALID_TYPES,
+    VARS_FILE,
+)
+
+# from cli.utils import *
+from cli.utils import (
+    api_get,
+    get_json_from_file,
+    get_template,
+    get_yaml_from_file,
+    input_single,
+    save_yaml_to_file,
+    validate_path_isdir,
+)
 
 logger = logging.getLogger()
 
@@ -28,7 +58,9 @@ class Parameter(BaseModel):
     @validator("type")
     def validate_type(cls, type):
         if type not in VALID_PARAMETER_VALUES:
-            raise ValueError(f"type must be one of: {list(VALID_PARAMETER_VALUES.keys())}")
+            raise ValueError(
+                f"type must be one of: {list(VALID_PARAMETER_VALUES.keys())}"
+            )
         return type
 
     @validator("value")
@@ -43,17 +75,21 @@ class Parameter(BaseModel):
                     v = VALID_PARAMETER_VALUES[type_](v)
                 except Exception:
                     if v is not None or values["required"]:
-                        raise ValueError(f"value must be of type {str(VALID_PARAMETER_VALUES[type_])}")
+                        raise ValueError(
+                            f"value must be of type {str(VALID_PARAMETER_VALUES[type_])}"
+                        )
             else:
                 if not isinstance(v, list):
-                    raise ValueError(f"value must be a list")
+                    raise ValueError("value must be a list")
                 try:
                     new_v = []
                     for v_ in v:
                         new_v.append(VALID_PARAMETER_VALUES[type_](v_))
                     v = new_v
                 except Exception as e:
-                    raise ValueError(f"the value in the list must be of type {str(VALID_PARAMETER_VALUES[type_])}")
+                    raise ValueError(
+                        f"the value in the list must be of type {str(VALID_PARAMETER_VALUES[type_])}"
+                    ) from e
         return v
 
 
@@ -66,19 +102,23 @@ class Spec(BaseModel):
     def validate_name(cls, name):
         invalid_characters = ["/", "-"]
         if not name[0].isupper():
-            raise ValueError(f"value's first letter must be capitalized")
+            raise ValueError("value's first letter must be capitalized")
         if any(x in name for x in invalid_characters):
-            raise Exception(f"value cannot contain any of these characters: {invalid_characters}")
+            raise Exception(
+                f"value cannot contain any of these characters: {invalid_characters}"
+            )
         return name
 
     @validator("version")
     def validate_version(cls, version):
         invalid_characters = ["/", "-"]
         if len(version) > 20:
-            raise Exception(f"value must be 20 characters maximum")
+            raise Exception("value must be 20 characters maximum")
 
         if any(x in version for x in invalid_characters):
-            raise Exception(f"value cannot contain any of these characters: {invalid_characters}")
+            raise Exception(
+                f"value cannot contain any of these characters: {invalid_characters}"
+            )
         return version
 
     @validator("parameters")
@@ -87,7 +127,9 @@ class Spec(BaseModel):
         for parameter in parameters:
             parameter_name = parameter.name
             if parameter_name in parameters_names:
-                raise Exception(f"Parameter name {parameter_name} is not unique")
+                raise Exception(
+                    f"Parameter name {parameter_name} is not unique"
+                )
             parameters_names.add(parameter_name)
         return parameters
 
@@ -102,6 +144,8 @@ class Component:
     version = None
     parameters = None
 
+    _STARTUP_FILEPAHT = "/tmp/ready_"
+
     def __init__(self, path, context):
         self.path = validate_path_isdir(os.path.abspath(path))
         self.vars_file = os.path.join(self.path, VARS_FILE)
@@ -112,18 +156,24 @@ class Component:
         validate_path_isdir(self.path)
         for required_file in REQUIRED_FILES:
             if not os.path.isfile(os.path.join(self.path, required_file)):
-                raise Exception(f"{required_file} file is missing in {self.path}")
+                raise Exception(
+                    f"{required_file} file is missing in {self.path}"
+                )
 
     def _validate_component(self):
         try:
             Spec.verify(self.spec)
             component_name = MAIN_CLASS_NAME
             if not hasattr(self.component, component_name):
-                raise Exception(f"Component does not have a class called {component_name}")
+                raise Exception(
+                    f"Component does not have a class called {component_name}"
+                )
 
             component_class = getattr(self.component, component_name)
             if not isinstance(component_class, Type):
-                raise Exception(f"There's no component class called {component_name}")
+                raise Exception(
+                    f"There's no component class called {component_name}"
+                )
 
         except Exception as e:
             raise Exception(f"Failed to validate component: {str(e)}")
@@ -149,27 +199,26 @@ class Component:
         for i, param in enumerate(self.spec["parameters"]):
             name = param["name"]
             if name in vars:
-                self.run_spec["parameters"][i]["value"] = vars[name]        
+                self.run_spec["parameters"][i]["value"] = vars[name]
         for key in extra_run_spec_fields.keys():
             self.run_spec[key] = vars[key]
 
-    def _prompt_run_spec_fields(self, reset_input, extra_run_spec_fields: dict):
+    def _prompt_run_spec_fields(
+        self, reset_input, extra_run_spec_fields: dict
+    ):
         Path(self.vars_file).touch()
         vars = get_yaml_from_file(self.vars_file)
         for i, param in enumerate(self.spec["parameters"]):
             name = param["name"]
             if reset_input or name not in vars:
-                param['value'] = vars.get(name, param['value'])
+                param["value"] = vars.get(name, param["value"])
                 vars[name] = input_single(param)
                 if param.get("multiple", False) and vars[name]:
-                    vars[name] = vars[name].split(',')
+                    vars[name] = vars[name].split(",")
         for key, default in extra_run_spec_fields.items():
             if reset_input or key not in vars:
                 vars[key] = click.prompt(
-                    key,
-                    type=str,
-                    default=default,
-                    show_default=True
+                    key, type=str, default=default, show_default=True
                 )
         save_yaml_to_file(payload=vars, file_path=self.vars_file)
 
@@ -179,7 +228,9 @@ class Component:
         try:
             subprocess.run(command, check=True, cwd=self.path, shell=True)
         except subprocess.CalledProcessError as e:
-            raise Exception(f"Failed to run command: {e.cmd}. Output: {e.output}")
+            raise Exception(
+                f"Failed to run command: {e.cmd}. Output: {e.output}"
+            )
 
     def _get_command_list(self) -> List[str]:
         initialization_file_path = os.path.join(self.path, INIT_FILE)
@@ -196,7 +247,10 @@ class Component:
     def _get_random_picture(self, path):
         # TODO REMOVE THIS.. MOVE TO HUBCLIENT IMPLEMENTATION
         user_handler = UserHandler(self.context)
-        file_data = api_get(f"{self.context.workspace.settings.SPLIGHT_HUB_API_HOST}/random_picture/", headers=user_handler.authorization_header)
+        file_data = api_get(
+            f"{self.context.workspace.settings.SPLIGHT_HUB_API_HOST}/random_picture/",
+            headers=user_handler.authorization_header,
+        )
         with open(path, "wb+") as f:
             f.write(file_data.content)
 
@@ -206,19 +260,30 @@ class Component:
         try:
             component = importlib.import_module(component_directory_name)
             main_class = getattr(component, MAIN_CLASS_NAME)
-            if not any([parent_class.__name__ == AbstractComponent.__name__ for parent_class in main_class.__mro__]):
-                raise Exception(f"Component class {MAIN_CLASS_NAME} must inherit from one of Splight's component classes")
+            if not any(
+                [
+                    parent_class.__name__ == AbstractComponent.__name__
+                    for parent_class in main_class.__mro__
+                ]
+            ):
+                raise Exception(
+                    f"Component class {MAIN_CLASS_NAME} must inherit from one of Splight's component classes"
+                )
             return component
         except Exception as e:
-            raise Exception(f"Failed importing component {component_directory_name}: {str(e)}")
+            raise Exception(
+                f"Failed importing component {component_directory_name}: {str(e)}"
+            )
 
     def initialize(self):
-        # TODO this should be removed from here. But it is present 
+        # TODO this should be removed from here. But it is present
         # to avoid to fail redinessprobe during req installation
+        self._save_startup_file(self._STARTUP_FILEPAHT)
+
         health_file = NamedTemporaryFile(prefix="healthy_")
-        logger.debug(f"Created healthy file")
+        logger.debug("Created healthy file")
         command_prefixes_map = {
-            "RUN": self. _command_run,
+            "RUN": self._command_run,
         }
 
         try:
@@ -243,11 +308,7 @@ class Component:
     def create(self, name, type, version):
         self._validate_type(type)
 
-        Spec.validate({
-            "name": name,
-            "version": version,
-            "parameters": []
-        })
+        Spec.validate({"name": name, "version": version, "parameters": []})
 
         self.path = os.path.join(self.path, f"{name}-{version}")
         os.mkdir(self.path)
@@ -262,9 +323,7 @@ class Component:
                 template_name = f"{type}.py"
             template: Template = get_template(template_name)
             file = template.render(
-                component_type=type,
-                component_name=name,
-                version=version
+                component_type=type, component_name=name, version=version
             )
             with open(file_path, "w+") as f:
                 f.write(file)
@@ -272,13 +331,15 @@ class Component:
     def push(self, type, force, public):
         self._validate_type(type)
         self._validate_component_structure()
-        self._load_spec() # TODO spec should have public attr
+        self._load_spec()  # TODO spec should have public attr
         self._load_component()
 
         handler = ComponentHandler(self.context)
         if not force and handler.exists_in_hub(type, self.name, self.version):
             raise ComponentAlreadyExistsException
-        handler.upload_component(type, self.name, self.version, self.parameters, public, self.path)
+        handler.upload_component(
+            type, self.name, self.version, self.parameters, public, self.path
+        )
 
     def pull(self, name, type, version):
         self._validate_type(type)
@@ -287,11 +348,15 @@ class Component:
         component_path = os.path.join(self.path, versioned_name)
 
         if os.path.exists(component_path):
-            raise Exception(f"Directory with name {versioned_name} already exists in path")
+            raise Exception(
+                f"Directory with name {versioned_name} already exists in path"
+            )
 
         handler = ComponentHandler(self.context)
         if not handler.exists_in_hub(type, name, version):
-            raise Exception(f"Component {versioned_name} does not exist in Splight Hub")
+            raise Exception(
+                f"Component {versioned_name} does not exist in Splight Hub"
+            )
 
         handler.download_component(type, name, version, self.path)
 
@@ -302,7 +367,9 @@ class Component:
 
         handler = ComponentHandler(self.context)
         if not handler.exists_in_hub(type, name, version):
-            raise Exception(f"Component {versioned_name} does not exist in Splight Hub")
+            raise Exception(
+                f"Component {versioned_name} does not exist in Splight Hub"
+            )
 
         handler.delete_component(type, name, version)
 
@@ -312,9 +379,9 @@ class Component:
         self._load_spec()
 
         extra_run_spec_fields = {
-            'namespace': DEFAULT_NAMESPACE,
-            'external_id': DEFAULT_EXTERNAL_ID,
-            'type': type.title(),
+            "namespace": DEFAULT_NAMESPACE,
+            "external_id": DEFAULT_EXTERNAL_ID,
+            "type": type.title(),
         }
         if run_spec:
             self.run_spec = json.loads(run_spec)
@@ -322,7 +389,7 @@ class Component:
             self.run_spec = self.spec
             self._prompt_run_spec_fields(
                 extra_run_spec_fields=extra_run_spec_fields,
-                reset_input=reset_input
+                reset_input=reset_input,
             )
             self._load_run_spec_fields(extra_run_spec_fields)
 
@@ -330,9 +397,16 @@ class Component:
 
         component_class = getattr(self.component, MAIN_CLASS_NAME)
         component = component_class(
-            instance_id=self.run_spec['external_id'],
-            namespace=self.run_spec['namespace'],
-            run_spec=json.dumps(self.run_spec)
+            instance_id=self.run_spec["external_id"],
+            namespace=self.run_spec["namespace"],
+            run_spec=json.dumps(self.run_spec),
         )
         component.setup = self.context.workspace.settings.dict()
         component.start()
+
+    @staticmethod
+    def _save_startup_file(file_path: str):
+        logger.debug("Creating startup file")
+        with open(file_path, "w") as fid:
+            fid.write("Component is ready for execution")
+        logger.debug("Startup file created")
