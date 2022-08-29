b86385e4681b312bab4dba23c9bc1dfffbbe215a
marianogaragiola <marianogaragiola@gmail.com>
2022-08-29 11:48:51 -0300

    last fix for utests

--- cli/tests/TestHub/__init__.py
+++ cli/tests/TestHub/__init__.py
@@ -2,6 +2,10 @@ from splight_lib.component import AbstractAlgorithmComponent
 
 
 class Main(AbstractAlgorithmComponent):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.start()
+
     def test(self):
         print("HELLO2")
 
