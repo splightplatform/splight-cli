20132cd80fec975157db141ff20529ba2e356256
marianogaragiola <marianogaragiola@gmail.com>
2022-10-24 17:01:40 -0300

    add support for input that has multiple values

--- cli/utils/input.py
+++ cli/utils/input.py
@@ -1,17 +1,40 @@
-import click
+from functools import partial
 from pydoc import locate
+from typing import List, Type, Union
+
+import click
+
+
+def list_of(input_value: Union[List, str], param_type: Type):
+    if isinstance(input_value, list):
+        return input_value
+    return [param_type(v.strip()) for v in input_value.split(",")]
+
 
 def input_single(param: dict):
-    default = 'None' if param['value'] is None and not param['required'] else param['value']
-    name = f"{'*' if param['required'] else ''}{param['name']}: {param['type']}"
+    default = (
+        "None"
+        if param["value"] is None and not param["required"]
+        else param["value"]
+    )
+    name = (
+        f"{'*' if param['required'] else ''}{param['name']}: {param['type']}"
+    )
+    print(param)
+    param_type = locate(param["type"])
+    value_proc = None
+    if param.get("multiple"):
+        value_proc = partial(list_of, param_type=param_type)
+
     val = click.prompt(
         name,
-        type=locate(param['type']),
+        type=param_type,
         default=default,
-        show_default=True
+        show_default=True,
+        value_proc=value_proc,
     )
     if isinstance(val, str):
         val = val.strip(" ")
-    if val == 'None':
+    if val == "None":
         val = None
     return val
