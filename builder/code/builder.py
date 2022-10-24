712881c61d1c290afa7b65a60f0c15662676c62b
Ignacio Fernandez <nachofernandez@Ignacios-MacBook-Pro.local>
2022-10-24 16:52:46 -0300

    changed script name in runner: builder.py > configure.py

--- builder/code/builder.py
+++ builder/code/builder.py
@@ -1,11 +1,36 @@
+from private_splight_lib.settings import setup
+from splight_lib import logging
+from splight_models import HubComponentVersion
+from pydantic import BaseModel
 import argparse
-from asyncio.log import logger
-import json
-import subprocess
+import docker
+from docker.errors import BuildError, APIError
+
+logger = logging.getLogger(__name__)
+
+
+class BuildSpec(BaseModel):
+    name: str
+    type: str
+    version: str
+    access_id: str
+    secret_key: str
+    workspace: str
+    registry: str
+
+
+def get_hub_client(build_spec: BuildSpec):
+    HubClient = setup.HUB_CLIENT
+    headers = {
+        "Authorization": f"Splight {build_spec.access_id} {build_spec.secret_key}"
+    }
+    return HubClient(headers=headers)
+
 
 if __name__ == '__main__':
+
     parser = argparse.ArgumentParser(
-        description='Executes a hub component.'
+        description='Builds a hub component image.'
     )
     parser.add_argument('-b',
                         '--build-spec',
@@ -13,7 +38,39 @@ if __name__ == '__main__':
                         nargs=1,
                         help='Build Spec',
                         required=True)
-
     args = parser.parse_args()
-    build_spec = json.loads(args.build_spec[0])
-    print(build_spec)
+    build_spec = BuildSpec.parse_raw(args.build_spec[0])
+    hub_client = get_hub_client(build_spec)
+
+    component_name = build_spec.name
+    component_version = build_spec.version.split("-")[1]
+    component_type = build_spec.type
+
+    component = hub_client.mine.get(HubComponentVersion, name=component_name, version=component_version, type=component_type.lower())[0]
+    component.build_status = "building"
+    hub_client.mine.update(HubComponentVersion, id=component.id, data=component.dict())
+
+    try:
+        tag = f"splight-runner:{build_spec.workspace}-{component_name}-{component_version}"
+        docker_client = docker.from_env()
+        logging.info("Building image")
+        docker_client.images.build(
+            path=".",
+            buildargs={
+                "BASE_IMAGE": f"{build_spec.registry}/splight-runner:{build_spec.workspace}",
+                "BUILD_SPEC": args.build_spec[0],
+            },
+            tag=tag
+        )
+
+        logging.info("Pushing image")
+        docker_client.images.push(build_spec.registry, tag)
+
+    except (APIError, BuildError) as e:
+        logger.exception(e)
+        component.build_status = "failed"
+        hub_client.mine.update(HubComponentVersion, id=component.id, data=component.dict())
+        exit(1)
+
+    component.build_status = "success"
+    hub_client.mine.update(HubComponentVersion, id=component.id, data=component.dict())
--- runner/configure.py
+++ runner/configure.py
@@ -7,29 +7,29 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Executes a hub component.'
     )
-    parser.add_argument('-b',
-                        '--build-spec',
+    parser.add_argument('-c',
+                        '--configure-spec',
                         type=str,
                         nargs=1,
-                        help='Build Spec',
+                        help='Configure Spec',
                         required=True)
 
     args = parser.parse_args()
-    build_spec = json.loads(args.build_spec[0])
+    configure_spec = json.loads(args.configure_spec[0])
     # TODO remove this overparsing
-    hub_type = build_spec["type"].lower()
-    hub_descriptor = build_spec["version"]
+    hub_type = configure_spec["type"].lower()
+    hub_descriptor = configure_spec["version"]
     hub_name, hub_version = hub_descriptor.split("-")
-    access_id = build_spec.get("access_id", None)
-    secret_key = build_spec.get("secret_key", None)
-    api_host = build_spec.get("api_host", None)
+    access_id = configure_spec.get("access_id", None)
+    secret_key = configure_spec.get("secret_key", None)
+    api_host = configure_spec.get("api_host", None)
 
     json_configuration = {
         "SPLIGHT_ACCESS_ID": access_id,
         "SPLIGHT_SECRET_KEY": secret_key,
         "SPLIGHT_PLATFORM_API_HOST": api_host,
     }
-    logger.info(f"Configure with {access_id} to build {hub_type} {hub_name} {hub_version}. Remote set to {api_host}")
+    logger.info(f"Configure with {access_id} to configure {hub_type} {hub_name} {hub_version}. Remote set to {api_host}")
     subprocess.run(["splightcli", "configure", "--from-json", json.dumps(json_configuration)], check=True)
     subprocess.run(["splightcli", "component", "pull", hub_type, hub_name, hub_version], check=True)
     subprocess.run(["splightcli", "component", "install-requirements", hub_type, hub_descriptor], check=True)
