48fd1bb7ffdba99786d4144e2e2d11b61bb70570
marianogaragiola <marianogaragiola@gmail.com>
2022-10-25 13:58:27 -0300

    handle the case for non python native types

--- cli/utils/input.py
+++ cli/utils/input.py
@@ -5,7 +5,7 @@ from typing import List, Type, Union
 import click
 
 
-def list_of(input_value: Union[List, str], param_type: Type):
+def list_of(input_value: Union[List, str], param_type: Type = str):
     if isinstance(input_value, list):
         return input_value
     return [param_type(v.strip()) for v in input_value.split(",")]
@@ -21,6 +21,8 @@ def input_single(param: dict):
         f"{'*' if param['required'] else ''}{param['name']}: {param['type']}"
     )
     param_type = locate(param["type"])
+    if not param_type:
+        param_type = str
     value_proc = None
     if param.get("multiple"):
         value_proc = partial(list_of, param_type=param_type)
