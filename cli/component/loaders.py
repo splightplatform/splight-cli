d429d83722717aa17a76d5716f5e2d3843a3f2d5
marianogaragiola <marianogaragiola@gmail.com>
2022-11-15 14:30:36 -0300

    missing required in input set as require

--- cli/component/loaders.py
+++ cli/component/loaders.py
@@ -67,7 +67,7 @@ class SpecJSONLoader:
             {
                 "name": f"{prefix}.{param_name}",
                 "type": param["type"],
-                "required": param["required"],
+                "required": param.get("required", True),
                 "multiple": param.get("multiple", False),
                 "value": param.get("value"),
             }
