5a021cd9b02e900457d65cc0f4f3a57ebf7cf5b6
Matias Silva <matias.silva@splight-ae.com>
2022-11-17 15:34:49 -0300

    Removed MyAsset pydantic from templates

--- cli/component/templates/algorithm.py
+++ cli/component/templates/algorithm.py
@@ -1,22 +1,10 @@
 import random
-from typing import Optional
-from pydantic import BaseModel
 from splight_lib.component.algorithms import AbstractAlgorithmComponent
 from splight_lib.execution import Task
 from splight_lib import logging
-from splight_models import Asset
 
 
 logger = logging.getLogger()
-
-
-# Custom Types
-class MyAsset(BaseModel):
-    id: str
-    name: str
-    description: Optional[str] = ""
-    asset: Asset
-    alias: str
 
 
 class Main(AbstractAlgorithmComponent):
@@ -44,21 +32,21 @@ class Main(AbstractAlgorithmComponent):
         self.datalake_client.save(instances=[out], collection=self.collection_name)
 
     def _list_assets(self):
-        logger.info(f"MyAssets: {self.my_assets}")
+        logger.info(f"My List[self.custom_type.MyAsset]: {self.my_assets}")
 
     # Bindings
-    def handle_myasset_create(self, my_asset: MyAsset):
+    def handle_myasset_create(self, my_asset):
         self.my_assets[my_asset.id] = my_asset
-        logger.info(f"CREATED MyAsset: {my_asset}")
+        logger.info(f"CREATED self.custom_type.MyAsset: {my_asset}")
 
-    def handle_myasset_delete(self, my_asset: MyAsset):
+    def handle_myasset_delete(self, my_asset):
         try:
-            logger.info(f"DELETED MyAsset: {my_asset}")
+            logger.info(f"DELETED self.custom_type.MyAsset: {my_asset}")
             del self.my_assets[my_asset.id]
         except KeyError:
             pass
 
     # Commands
-    def command_myasset_print(self, asset: MyAsset):
-        logger.info(f"PRINTED MyAsset: {asset}")
-        return asset.dict()
+    def command_myasset_print(self, myasset):
+        logger.info(f"PRINTED self.custom_type.MyAsset: {myasset}")
+        return myasset.dict()
