c11a22d089b6836d2657331800f4bd1fc5b181d1
Mariano Piatti <mariano.piatti@splight-ae.com>
2022-11-29 11:32:57 -0300

    remove unnecessary templates

--- cli/component/templates/network.py
+++
@@ -1,9 +0,0 @@
-from splight_lib.component.abstract import AbstractComponent
-
-class Main(AbstractComponent):
-
-    def __init__(self, *args, **kwargs):
-        super(Main, self).__init__(*args, **kwargs)
-    
-    def start(self):
-        pass
