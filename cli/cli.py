3aa5d7f36ee776465b1c2b29a928743e93739e77
marianogaragiola <marianogaragiola@gmail.com>
2023-01-10 15:35:08 -0300

    feat: move configure command

--- cli/cli.py
+++ cli/cli.py
@@ -1,10 +1,7 @@
-import sys
-
 import click
-from cli.context import *
-from cli.constants import *
-from cli.settings import *
-from cli.utils import *
+from click_default_group import DefaultGroup
+
+from cli.context import Context, pass_context, needs_credentials
 from cli.version import __version__
 
 
@@ -56,29 +53,7 @@ def workspace(ctx):
     ctx.obj = Context()
 
 
-@cli.command()
-@click.option("-j", "--from-json", help="Configuration by json instaed of prompt.")
+@cli.group(cls=DefaultGroup, default="config", default_if_no_args=True)
 @pass_context
-def configure(ctx: Context, from_json=False) -> None:
+def configure(ctx: Context, from_json: str = False) -> None:
     ctx.obj = Context()
-    try:
-        if from_json:
-            from_json = json.loads(from_json)
-            new_settings = SplightCLISettings.parse_obj(from_json)
-        else:
-            new_settings_data = {}
-            for config_var in CONFIG_VARS:
-                default = getattr(ctx.workspace.settings, config_var, None)
-                value = click.prompt(
-                    click.style(config_var, fg='yellow'),
-                    type=str,
-                    default=default,
-                    show_default=True
-                )
-                new_settings_data.update({config_var: value})
-            new_settings = SplightCLISettings.parse_obj(new_settings_data)
-        ctx.workspace.update_workspace(new_settings)
-        click.secho("Configuration saved successfully", fg="green")
-    except Exception as e:
-        click.secho(f"Error configuring Splight CLI: {str(e)}", fg="red")
-        sys.exit(1)
