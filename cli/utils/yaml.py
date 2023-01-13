0e019b684d6ba31b5c9cdc37a56d3c2ab9094426
marianogaragiola <marianogaragiola@gmail.com>
2023-01-13 09:25:17 -0300

    minor changes in workspace manager, still in progress

--- cli/utils/yaml.py
+++ cli/utils/yaml.py
@@ -1,6 +1,7 @@
 import os
 import yaml
 
+
 def get_yaml_from_file(file_path: str):
     if not os.path.isfile(file_path):
         raise Exception(f"String provided: \"{file_path}\" is not a file, please provide a filepath or use -fs flag")
@@ -9,9 +10,10 @@ def get_yaml_from_file(file_path: str):
             data = yaml.load(f, Loader=yaml.FullLoader)
             return data if data is not None else {}
         except yaml.YAMLError as e:
-            raise Exception("File is not a valid YAML")
+            raise Exception("File is not a valid YAML") from e
+
 
 def save_yaml_to_file(payload: str, file_path: str):
     with open(file_path, 'w+') as f:
-        yaml.Dumper.ignore_aliases = lambda *args : True # TOO RISKY to add refs on repetitions
+        yaml.Dumper.ignore_aliases = lambda *args : True  # TOO RISKY to add refs on repetitions
         yaml.dump(payload, f)
