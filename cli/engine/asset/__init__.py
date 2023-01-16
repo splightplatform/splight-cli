f5b908aa198991aca251a63a9cca90c70294fde3
marianogaragiola <marianogaragiola@gmail.com>
2023-01-16 12:35:59 -0300

    feat: asset commands using new table printer

--- cli/engine/asset/__init__.py
+++ cli/engine/asset/__init__.py
@@ -1,16 +1,19 @@
 import typer
+from rich.console import Console
+from rich.table import Table
 from splight_models import Asset
 from tabulate import tabulate
 
 from cli.engine.endpoint import APIEndpoint
-from cli.utils.headers import extract_headers
 
 asset_app = typer.Typer(
     name="Splight Engine Asset",
     add_completion=True,
     rich_markup_mode="rich",
+    no_args_is_help=True,
 )
 
+console = Console()
 PATH = "asset/"
 
 
@@ -28,14 +31,13 @@ def list(
         client=ctx.obj.framework.setup.DATABASE_CLIENT(),
         model=Asset,
     )
-    instances = endpoint.list(limit=limit)
-    table = tabulate(
-        [[item.id, item.name] for item in instances],
-        headers=["id", "name"],
-        showindex=True,
-        stralign="left",
-    )
-    print(table)
+    instances = endpoint.list(skip=skip, limit=limit)
+    table = Table("", "ID", "Name")
+    _ = [
+        table.add_row(str(counter), item.id, item.name)
+        for counter, item in enumerate(instances)
+    ]
+    console.print(table)
 
 
 @asset_app.command()
@@ -48,7 +50,12 @@ def get(
         model=Asset,
     )
     instance = endpoint.get(instance_id)
-    print(tabulate([[key, value] for key, value in instance.dict().items()]))
+    table = Table(title=f"Asset = {instance.name}", show_header=False)
+    _ = [
+        table.add_row(key, str(value))
+        for key, value in instance.dict().items()
+    ]
+    console.print(table)
 
 
 @asset_app.command()
