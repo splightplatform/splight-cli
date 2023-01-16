b3a8886e08040e137a6c7f38340808a2730f3ae4
marianogaragiola <marianogaragiola@gmail.com>
2023-01-16 12:23:08 -0300

    remove unused callback function

--- cli/engine/__init__.py
+++ cli/engine/__init__.py
@@ -7,11 +7,7 @@ engine_app = typer.Typer(
     name="Splight Engine",
     add_completion=True,
     rich_markup_mode="rich",
+    no_args_is_help=True,
 )
 
 engine_app.add_typer(asset_app, name="asset")
-
-
-@engine_app.callback(invoke_without_command=True)
-def engine_callback(ctx: typer.Context):
-    pass
