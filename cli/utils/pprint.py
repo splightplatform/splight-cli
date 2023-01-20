c2cbdb3be177bca14bf577ac609e251173afc55e
marianogaragiola <marianogaragiola@gmail.com>
2023-01-20 16:58:37 -0300

    using typer for printing

--- cli/utils/pprint.py
+++ cli/utils/pprint.py
@@ -1,33 +1,36 @@
-import click
+# import click
+import typer
 
 
 class Printer:
-    DEFAULT_COLOR = 'white'
+    DEFAULT_COLOR = "white"
 
     @staticmethod
     def print_dict(items, headers, items_colors=[]):
         format_str = "{:<50}" * len(headers)
-        click.secho(format_str.format(*[h.upper() for h in headers]))
+        typer.echo(format_str.format(*[h.upper() for h in headers]))
         for index, item in enumerate(items):
             try:
                 fg = items_colors[index]
             except IndexError:
                 fg = Printer.DEFAULT_COLOR
-            click.secho(format_str.format(*[item.get(key) for key in headers]))
+            typer.echo(
+                format_str.format(*[item.get(key) for key in headers]),
+                color=fg,
+            )
 
     @staticmethod
     def print_list(items, header=None, items_colors=[]):
         if header is not None:
-            click.secho(header.upper())
+            typer.echo(header.upper())
         for index, item in enumerate(items):
             try:
                 fg = items_colors[index]
             except IndexError:
                 fg = Printer.DEFAULT_COLOR
-            click.secho(item, fg=fg)
-    
+            typer.echo(item, color=fg)
+
     @staticmethod
     def print_value(item, color=None):
         fg = color if color is not None else Printer.DEFAULT_COLOR
-        click.secho(item, fg)
-
+        typer.echo(item, color=fg)
