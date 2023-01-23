55d9d3a663061670ae74c99831396f706423ce45
marianogaragiola <marianogaragiola@gmail.com>
2023-01-23 11:18:48 -0300

    style

--- cli/component/component.py
+++ cli/component/component.py
@@ -1,19 +1,21 @@
-import uuid
 import os
-from typing import List, Optional, Dict
-from jinja2 import Template
-from splight_lib.execution import Thread
-from splight_lib import logging
-from cli.version import __version__
-from cli.constants import (
-    COMPONENT_FILE,
-)
-from cli.utils import get_template
-from cli.component.handler import ComponentHandler
-from cli.component.spec import Spec
-from cli.component.loaders import SpecLoader, ComponentLoader, InitLoader
-from cli.component.exceptions import ComponentAlreadyExistsException, ComponentDirectoryAlreadyExists
+import uuid
+from typing import Dict, List, Optional
 
+from jinja2 import Template
+from splight_lib import logging
+from splight_lib.execution import Thread
+
+from cli.component.exceptions import (
+    ComponentAlreadyExistsException,
+    ComponentDirectoryAlreadyExists,
+)
+from cli.component.handler import ComponentHandler
+from cli.component.loaders import ComponentLoader, InitLoader, SpecLoader
+from cli.component.spec import Spec
+from cli.constants import COMPONENT_FILE
+from cli.utils import get_template
+from cli.version import __version__
 
 logger = logging.getLogger()
 
@@ -34,15 +36,17 @@ class Component:
         return handler.list_component_versions(name)
 
     def create(self, name, version):
-        Spec.verify({
-            "name": name,
-            "version": version,
-            "splight_cli_version": __version__,
-            "custom_types": [],
-            "input": [],
-            "output": [],
-            "endpoints": [],
-        })
+        Spec.verify(
+            {
+                "name": name,
+                "version": version,
+                "splight_cli_version": __version__,
+                "custom_types": [],
+                "input": [],
+                "output": [],
+                "endpoints": [],
+            }
+        )
 
         component_path = os.path.join(f"{name}", f"{version}")
         os.makedirs(component_path)
@@ -58,7 +62,7 @@ class Component:
                 component_name=name,
                 version=version,
                 component_id=component_id,
-                splight_cli_version=__version__
+                splight_cli_version=__version__,
             )
             with open(file_path, "w+") as f:
                 f.write(file)
@@ -77,28 +81,35 @@ class Component:
         handler = ComponentHandler(self.context)
         handler.delete_component(name, version)
 
-    def push(self, path: str, force = False):
-        # Load json and validate Spec structure 
+    def push(self, path: str, force=False):
+        # Load json and validate Spec structure
         loader = SpecLoader(path=path)
         spec = loader.load(prompt_input=False)
 
         # TODO simplify this
         handler = ComponentHandler(self.context)
         if not force and handler.exists_in_hub(spec.name, spec.version):
-            raise ComponentAlreadyExistsException(f'{spec.name}-{spec.version}')
+            raise ComponentAlreadyExistsException(
+                f"{spec.name}-{spec.version}"
+            )
         handler.upload_component(spec, local_path=path)
 
-    def run(self, path: str, input_parameters: Optional[List[Dict]] = None, component_id: str = None):
-        # Load py module and validate Splight Component structure 
+    def run(
+        self,
+        path: str,
+        input_parameters: Optional[List[Dict]] = None,
+        component_id: str = None,
+    ):
+        # Load py module and validate Splight Component structure
         loader = ComponentLoader(path=path)
         component_class = loader.load()
-        # Load json and validate Spec structure 
+        # Load json and validate Spec structure
         loader = SpecLoader(path=path)
         run_spec = loader.load(input_parameters=input_parameters)
         component = component_class(
             run_spec=run_spec.dict(),
             initial_setup=self.context.workspace.settings.dict(),
-            component_id=component_id
+            component_id=component_id,
         )
         component.execution_client.start(Thread(target=component.start))
 
