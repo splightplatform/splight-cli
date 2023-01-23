6c876938b7ddbb85839c4646dca6f5be2adb6b73
marianogaragiola <marianogaragiola@gmail.com>
2023-01-23 09:21:49 -0300

    fix conflicts

--- cli/cli.py
+++ cli/cli.py
@@ -1,52 +1,76 @@
-import click
-from click_default_group import DefaultGroup
+from typing import Optional
 
-from cli.context import Context, pass_context, needs_credentials
+import typer
+
 from cli.version import __version__
+from cli.context import Context
+from cli.config import config_app
 
 
-@click.group()
-@click.version_option(version=__version__, message="%(version)s")
-@click.pass_context
-def cli(ctx):
+app = typer.Typer(
+    name="Splight Command Line",
+    add_completion=True,
+    rich_markup_mode="rich",
+)
+
+app.add_typer(config_app, name="configure")
+
+
+def version_callback(version: bool):
+    if version:
+        print(__version__)
+        raise typer.Exit()
+
+
+@app.callback()
+def main(
+    ctx: typer.Context,
+    version: Optional[bool] = typer.Option(
+        None,
+        "--version",
+        "-v",
+        callback=version_callback,
+        is_eager=True
+    )
+):
     ctx.obj = Context()
 
 
-@cli.group()
-@pass_context
-@needs_credentials
-def component(ctx):
-    ctx.obj = Context()
-
-
-@cli.group()
-@pass_context
-@needs_credentials
-def datalake(ctx):
-    ctx.obj = Context()
-
-
-@cli.group()
-@pass_context
-@needs_credentials
-def database(ctx):
-    ctx.obj = Context()
-
-
-@cli.group()
-@pass_context
-@needs_credentials
-def deployment(ctx):
-    ctx.obj = Context()
-
-
-@cli.group()
-@pass_context
-def workspace(ctx):
-    ctx.obj = Context()
-
-
-@cli.group(cls=DefaultGroup, default="config", default_if_no_args=True)
-@pass_context
-def configure(ctx: Context, from_json: str = False) -> None:
-    ctx.obj = Context()
+# @cli.group()
+# @pass_context
+# @needs_credentials
+# def component(ctx):
+#     ctx.obj = Context()
+#
+#
+# @cli.group()
+# @pass_context
+# @needs_credentials
+# def datalake(ctx):
+#     ctx.obj = Context()
+#
+#
+# @cli.group()
+# @pass_context
+# @needs_credentials
+# def database(ctx):
+#     ctx.obj = Context()
+#
+#
+# @cli.group()
+# @pass_context
+# @needs_credentials
+# def deployment(ctx):
+#     ctx.obj = Context()
+#
+#
+# @cli.group()
+# @pass_context
+# def workspace(ctx):
+#     ctx.obj = Context()
+#
+#
+# @cli.group(cls=DefaultGroup, default="config", default_if_no_args=True)
+# @pass_context
+# def configure(ctx: Context, from_json: str = False) -> None:
+#     ctx.obj = Context()
