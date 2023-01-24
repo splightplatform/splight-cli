db04b8919d3c1befdf90345d3ea2c36594c92312
marianogaragiola <marianogaragiola@gmail.com>
2023-01-24 10:25:15 -0300

    fix unittest

--- cli/tests/test_configure.py
+++ cli/tests/test_configure.py
@@ -1,31 +1,35 @@
 from unittest.mock import patch
-from cli.cli import component
-from cli.tests.test_generic import SplightCLITest
-from cli.constants import *
+
+from cli.cli import component_app
+
 from cli.component.handler import ComponentHandler
+from cli.tests.test_generic import SplightCLITest
 
 
 class TestConfigure(SplightCLITest):
-
     def test_configure_requested(self):
-        with patch.object(ComponentHandler, "list_components", return_value=[]):
+        with patch.object(
+            ComponentHandler, "list_components", return_value=[]
+        ):
             result = self.runner.invoke(
-                component,
+                component_app,
                 ["list"],
                 obj=self.context,
-                catch_exceptions=False
+                catch_exceptions=False,
             )
         self.assertEqual(result.exit_code, 1)
-        self.assertIn("splightcli configure", result.output)
+        self.assertIn("splight configure", result.output)
 
     def test_configure_not_requested(self):
         self.configure()
-        with patch.object(ComponentHandler, "list_components", return_value=[]):
+        with patch.object(
+            ComponentHandler, "list_components", return_value=[]
+        ):
             result = self.runner.invoke(
-                component,
+                component_app,
                 ["list"],
                 obj=self.context,
-                catch_exceptions=False
+                catch_exceptions=False,
             )
         self.assertEqual(result.exit_code, 0)
-        self.assertIn('NAME', result.output)
+        self.assertIn("Name", result.output)
