5ca777436778ad4df6d3b5efe9954f45f1984fa8
marianogaragiola <marianogaragiola@gmail.com>
2023-01-24 09:17:08 -0300

    feat: add function for validating credentials

--- cli/context/__init__.py
+++ cli/context/__init__.py
@@ -1,7 +1,15 @@
-import click
-from functools import wraps
-from cli.context.workspace import WorkspaceManager
+# import click
+import typer
+
+# from functools import wraps
+from rich.console import Console
+
+from cli.constants import error_style
 from cli.context.framework import FrameworkManager
+from cli.context.workspace import WorkspaceManager
+
+console = Console()
+
 
 class Context:
     def __init__(self):
@@ -21,19 +29,33 @@ class Context:
         return f"<Context {self.__workspace}>"
 
 
-pass_context = click.make_pass_decorator(Context)
+# pass_context = click.make_pass_decorator(Context)
 
 
-def needs_credentials(f):
-    @wraps(f)
-    def wrapper(ctx, *args, **kwargs):
-        access_id = ctx.workspace.settings.SPLIGHT_ACCESS_ID
-        secret_key = ctx.workspace.settings.SPLIGHT_SECRET_KEY
-        if access_id is None or secret_key is None:
-            click.secho(
-                "Please set your Splight credentials. Use \"splightcli configure\"",
-                fg="red"
-            )
-            exit(1)
-        return f(ctx, *args, **kwargs)
-    return wrapper
+def check_credentials(ctx: typer.Context):
+    settings = ctx.obj.workspace.settings
+
+    access_id = settings.SPLIGHT_ACCESS_ID
+    secret_key = settings.SPLIGHT_SECRET_KEY
+    if access_id is None or secret_key is None:
+        console.print(
+            'Please set you Splight credentials with "splight configure"',
+            style=error_style,
+        )
+        raise typer.Exit(1)
+
+
+# def needs_credentials(f):
+#     @wraps(f)
+#     def wrapper(ctx: typer.Context, *args, **kwargs):
+#         __import__('ipdb').set_trace()
+#         access_id = ctx.workspace.settings.SPLIGHT_ACCESS_ID
+#         secret_key = ctx.workspace.settings.SPLIGHT_SECRET_KEY
+#         if access_id is None or secret_key is None:
+#             click.secho(
+#                 "Please set your Splight credentials. Use \"splightcli configure\"",
+#                 fg="red"
+#             )
+#             exit(1)
+#         return f(ctx, *args, **kwargs)
+#     return wrapper
