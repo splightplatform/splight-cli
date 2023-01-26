9ab18676703ca49b4e692828d60c3905f7e2f792
Ignacio Fernandez <nachofernandez@Ignacios-MacBook-Pro.local>
2023-01-26 10:46:11 -0800

    engine datalake command finished

--- cli/engine/datalake/__init__.py
+++ cli/engine/datalake/__init__.py
@@ -1,10 +1,11 @@
-import json
+from collections import defaultdict
+from typing import List
 
 import typer
 from rich.console import Console
-from splight_models import Asset
+from splight_models import Asset, Component
 from cli.constants import error_style
-from cli.engine.manager import DatalakeManager
+from cli.engine.manager import DatalakeManager, DatalakeManagerException
 
 datalake_app = typer.Typer(
     name="Splight Engine Datalake",
@@ -12,51 +13,78 @@ datalake_app = typer.Typer(
     rich_markup_mode="rich",
     no_args_is_help=True,
 )
+console = Console()
+
+
+def _parse_filter_option(values):
+    result = {}
+    for value in values:
+        k, v = value.split('=')
+        result[k] = v
+    return result
 
 
 @datalake_app.command()
 def list(
     ctx: typer.Context,
-    # skip: int = typer.Option(
-    #     0, "--skip", "-s", help="Number of element to skip"
-    # ),
-    # limit: int = typer.Option(
-    #     -1, "--limit", "-l", help="Limit the number of listed elements"
-    # ),
+    skip: int = typer.Option(
+        0, "--skip", "-s", help="Number of element to skip"
+    ),
+    limit: int = typer.Option(
+        -1, "--limit", "-l", help="Limit the number of listed elements"
+    ),
 ):
-
     manager = DatalakeManager(
-        client=ctx.obj.framework.setup.DATALAKE_CLIENT(),
+        db_client=ctx.obj.framework.setup.DATABASE_CLIENT(),
+        dl_client=ctx.obj.framework.setup.DATALAKE_CLIENT(),
     )
-    manager.list()
-
-
-@datalake_app.command()
-def load(
-    ctx: typer.Context,
-    instance_id: str = typer.Argument(..., help="The Asset's ID"),
-):
-    manager = ResourceManager(
-        client=ctx.obj.framework.setup.DATABASE_CLIENT(),
-        model=MODEL,
-    )
-    try:
-        manager.get(instance_id)
-    except ResourceManagerException as exc:
-        console.print(exc, style=error_style)
+    manager.list(skip, limit)
 
 
 @datalake_app.command()
 def dump(
     ctx: typer.Context,
-    path: str = typer.Argument(
-        ..., help="Path to JSON file with resource data"
+    collection: str = typer.Argument(
+        ..., help="Collection name to dump"
+    ),
+    path: str = typer.Option(
+        './dump.csv', "--path", "-p", help="Path name to dump"
+    ),
+    filter: List[str] = typer.Option(
+        None, "--filter", "-f", help="Filter to apply"
     ),
 ):
-    manager = ResourceManager(
-        client=ctx.obj.framework.setup.DATABASE_CLIENT(),
-        model=MODEL,
+    filters = _parse_filter_option(filter)
+    manager = DatalakeManager(
+        db_client=ctx.obj.framework.setup.DATABASE_CLIENT(),
+        dl_client=ctx.obj.framework.setup.DATALAKE_CLIENT(),
     )
-    with open(path, "r") as fid:
-        body = json.load(fid)
-    manager.create(data=body)
+    try:
+        manager.dump(
+            collection=collection,
+            path=path,
+            filters=filters
+        )
+    except DatalakeManagerException as exc:
+        console.print(exc, style=error_style)
+
+
+@datalake_app.command()
+def load(
+    ctx: typer.Context,
+    collection: str = typer.Argument(..., help="Collection name to load"),
+    path: str = typer.Option(
+        ..., "--path", "-p", help="Path to file to load"
+    ),
+):
+    manager = DatalakeManager(
+        db_client=ctx.obj.framework.setup.DATABASE_CLIENT(),
+        dl_client=ctx.obj.framework.setup.DATALAKE_CLIENT(),
+    )
+    try:
+        manager.load(
+            collection=collection,
+            path=path
+        )
+    except DatalakeManagerException as exc:
+        console.print(exc, style=error_style)
