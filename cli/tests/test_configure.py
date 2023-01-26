5af91fcb97b7b869a993fadd2379444264ca1b99
marianogaragiola <marianogaragiola@gmail.com>
2023-01-26 09:21:13 -0300

    update unittest

--- cli/tests/test_configure.py
+++ cli/tests/test_configure.py
@@ -1,35 +1,39 @@
+import re
 from unittest.mock import patch
 
-from cli.cli import component_app
+# from cli.hub.component import component_app
+from cli.hub import hub_app
+from cli.hub.component.hub_manager import HubComponentManager
 
-from cli.component.handler import ComponentHandler
+# from cli.component.handler import ComponentHandler
 from cli.tests.test_generic import SplightCLITest
 
 
 class TestConfigure(SplightCLITest):
     def test_configure_requested(self):
         with patch.object(
-            ComponentHandler, "list_components", return_value=[]
+            HubComponentManager, "list_components", return_value=None
         ):
             result = self.runner.invoke(
-                component_app,
-                ["list"],
+                hub_app,
+                ["component", "list"],
                 obj=self.context,
                 catch_exceptions=False,
             )
+        ansi_escape = re.compile(r"\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])")
+        output = ansi_escape.sub("", result.output)
         self.assertEqual(result.exit_code, 1)
-        self.assertIn("splight configure", result.output)
+        self.assertIn("splight configure", output)
 
     def test_configure_not_requested(self):
         self.configure()
         with patch.object(
-            ComponentHandler, "list_components", return_value=[]
+            HubComponentManager, "list_components", return_value=None
         ):
             result = self.runner.invoke(
-                component_app,
-                ["list"],
+                hub_app,
+                ["component", "list"],
                 obj=self.context,
                 catch_exceptions=False,
             )
         self.assertEqual(result.exit_code, 0)
-        self.assertIn("Name", result.output)
