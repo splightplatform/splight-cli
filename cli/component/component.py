06a7c5c29b7fc17a3382130677ac42977e40b15a
Lautaro Lecumberry <lautarolecumberry@gmail.com>
2023-03-06 13:40:18 -0300

    fix creating component with no path

--- cli/component/component.py
+++ cli/component/component.py
@@ -43,11 +43,13 @@ class Component:
             }
         )
 
-        if not os.path.exists(component_path):
+        if component_path and not os.path.exists(component_path):
             component_path = os.path.join(f"{component_path}")
             os.makedirs(component_path)
 
+
         for file_name in ComponentLoader.REQUIRED_FILES:
+
             template_name = file_name
             file_path = os.path.join(component_path, file_name)
             component_id = str(uuid.uuid4())
