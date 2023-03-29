116f4111c0e4e15f27cd200a79ddaf9affddb624
Lautaro Lecumberry <lautarolecumberry@gmail.com>
2023-03-29 11:56:25 -0300

    add macro like values

--- .github/workflows/check_version.py
+++ .github/workflows/check_version.py
@@ -5,17 +5,22 @@ import requests
 import sys
 
 
-def get_public_version(project_name: str):
-    response = requests.get(f'https://pypi.org/pypi/{project_name}/json')
-    response.raise_for_status()
-    return version.parse(json.loads(response.content)['info']['version'])
+PROJECT_NAME = "splight-cli"
+VERSION_PATH = "cli/version.py"
 
-if __name__ == '__main__':
-    with open("cli/version.py", "r") as f:
+
+def get_public_version(project_name: str):
+    response = requests.get(f"https://pypi.org/pypi/{project_name}/json")
+    response.raise_for_status()
+    return version.parse(json.loads(response.content)["info"]["version"])
+
+
+if __name__ == "__main__":
+    with open(VERSION_PATH, "r") as f:
         string_version = f.readline().split('"')[1]
 
     project_version = version.parse(string_version)
-    public_project_version = get_public_version("splight-cli")
+    public_project_version = get_public_version(PROJECT_NAME)
 
     if project_version < public_project_version:
         raise Exception(
