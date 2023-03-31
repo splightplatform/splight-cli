25fae5ab81d9e4a4ac77b0278d3269c95bcc064e
marianogaragiola <marianogaragiola@gmail.com>
2023-03-31 17:59:54 -0300

    delete unused code

--- cli/utils/__init__.py
+++ cli/utils/__init__.py
@@ -1,8 +1,8 @@
 from cli.utils.template import *
-from cli.utils.validation import *
+# from cli.utils.validation import *
 from cli.utils.json import *
-from cli.utils.api_requests import *
+# from cli.utils.api_requests import *
 from cli.utils.yaml import *
 from cli.utils.input import *
 from cli.utils.pprint import *
-from cli.utils.uuid import *
+# from cli.utils.uuid import *
--- cli/utils/validation.py
+++
@@ -1,6 +0,0 @@
-import os
-
-def validate_path_isdir(path: str) -> str:
-    if not os.path.isdir(path):
-        raise ValueError(f"Path provided: \"{path}\" is not a directory")
-    return path
