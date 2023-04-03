91dea93f97aab19fc6f79e7075f5e07a17ed9e03
Lautaro Lecumberry <lautarolecumberry@gmail.com>
2023-04-03 17:41:23 -0300

    add output

--- .github/workflows/check_branch_version.py
+++ .github/workflows/check_branch_version.py
@@ -9,15 +9,18 @@ def get_version_from_line(line):
 
 if __name__ == "__main__":
     branch_name = sys.argv[1]
-
-    print("\n\n\n\n\n", branch_name, "\n\n\n\n\n")
     cmd = ["git", "diff", f"master..{branch_name}", "--", "cli/version.py"]
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
     output, error = process.communicate()
     output_lines = output.decode().split("\n")
 
-    old_version_line = output_lines[-3]
-    new_version_line = output_lines[-2]
+    try:
+        old_version_line = output_lines[-3]
+        new_version_line = output_lines[-2]
+    except:
+        print("bn", branch_name)
+        print("argv", sys.argv)
+        print("l", output_lines)
 
     old_version = get_version_from_line(old_version_line)
     new_version = get_version_from_line(new_version_line)
