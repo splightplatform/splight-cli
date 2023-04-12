215e2ba9a32f22bbcdcc4699581afea7e6ba5ec5
Agustin Curto <agustin.curto@splight-ai.com>
2023-04-12 14:39:43 -0300

    Define testing package. Add testing dependencies

---
+++ cli/testing/__init__.py
@@ -0,0 +1,46 @@
+import pytest
+from cli.component.loaders import ComponentLoader, SpecLoader
+
+
+def get_tests_initial_setup() -> dict:
+    # TODO: this will be change when we refactor setup.configure to don't override envvars
+    return {
+        "DATABASE_CLIENT": "splight_lib.client.database.LocalDatabaseClient",
+        "DATALAKE_CLIENT": "splight_lib.client.datalake.LocalDatalakeClient",
+        "NAMESPACE": "test",
+        # TODO: uncomment following line when local communication client will be ready
+        # "COMMUNICATION_CLIENT": "splight_lib.client.communication.LocalCommunicationClient"
+    }
+
+
+@pytest.fixture
+def component(mocker):
+    # we don't need execution client to run locally
+    mocker.patch("splight_lib.component.abstract.ExecutionClient")
+
+    component_path = "."
+    clients_config = {"path": component_path}
+    # local database, datalake and communication clients
+    initial_setup = get_tests_initial_setup()
+
+    # TODO: remove, just temportal until define LocalCommunicationClient
+    mocker.patch("remote_splight_lib.communication.client.CommunicationClient")
+    initial_setup["COMMUNICATION_CLIENT"] = "splight_lib.client.datalake.LocalDatalakeClient"
+    mocker.patch("splight_lib.component.abstract.AbstractComponent._load_instance_kwargs_for_clients")
+    mocker.patch("splight_lib.component.abstract.AbstractComponent.communication_client_kwargs", clients_config)
+    mocker.patch("splight_lib.component.abstract.BindingsMixin._load_client_bindings")
+
+    component_loader = ComponentLoader(path=component_path)
+    spec_loader = SpecLoader(path=component_path)
+
+    component_class = component_loader.load()
+    run_spec = spec_loader.load(input_parameters={}).dict()
+
+    component = component_class(
+        run_spec=run_spec,
+        initial_setup=initial_setup,
+        database_config=clients_config,
+        datalake_config=clients_config,
+    )
+
+    return component
