c70adc29884b95a8b171ab2e5ebe421d5197a2a6
Agustin Curto <agustin.curto@splight-ai.com>
2023-05-09 16:19:40 -0300

    Add new dev pre-release and release lib workflows. Update version

--- .github/workflows/scripts/check_pypi_version.py
+++ .github/workflows/scripts/check_pypi_version.py
@@ -1,47 +1,39 @@
 import json
 import sys
-from packaging.version import parse
-import requests
+import urllib.request
+
+from pkg_resources import parse_version as parse
 
 
-class VersionError(Exception):
-    """Raised when the version is not greater than the one on pypi"""
-
-    pass
+class PypiVersionError(Exception):
+    """Raised when the version is not greater than the one on pypi."""
 
 
 def get_pypi_version(project_name: str):
     url = f"https://pypi.org/pypi/{project_name}/json"
-    response = requests.get(url)
-    response.raise_for_status()
-    version = json.loads(response.content)["info"]["version"]
+    response = urllib.request.urlopen(url)
+    version = json.loads(response.read())["info"]["version"]
     return parse(version)
 
 
 if __name__ == "__main__":
-    """Compares versions between the local repository
-    and the package on pypi.
+    """Compares versions between the local repository and the package on pypi.
 
     Args:
-        version_file (arg 1) (str): path to the version file
-            must be a one liner containing the version
-            number between double quotes.
+        local_version (arg 1) (str): local version number (x.y.z)
         project_name (arg 2) (str): name of the project on pypi
     Raises:
-        VersionError: if the local version is not greater than
-            the one on pypi
+        PypiVersionError: if the local version is not greater than the one on
+        pypi
     """
-    version_file = sys.argv[1]
+    local_version = sys.argv[1]
     project_name = sys.argv[2]
 
-    with open(version_file, "r") as f:
-        string_version = f.readline().split('"')[1]
-
-    project_version = parse(string_version)
+    project_version = parse(local_version)
     public_project_version = get_pypi_version(project_name)
 
     if project_version <= public_project_version:
-        raise VersionError(
+        raise PypiVersionError(
             f"Feature version {project_version} is not greater than "
             f"uploaded version {public_project_version}"
         )
