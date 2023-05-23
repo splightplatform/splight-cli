195a3373a5eb62fe20eade1ddccba339fd5b9692
marianogaragiola <marianogaragiola@gmail.com>
2023-05-23 19:54:22 -0300

    some docstrings

--- cli/component/component.py
+++ cli/component/component.py
@@ -12,13 +12,13 @@ from splight_lib.component.spec import Spec
 from strenum import LowercaseStrEnum
 
 from cli.component.exceptions import (
+    ComponentExecutionError,
     InvalidSplightCLIVersion,
     ReadmeExists,
-    ComponentExecutionError,
+    ComponentTestFileDoesNotExists,
+    ComponentTestError,
 )
-from cli.component.loaders import (
-    InitLoader,
-)
+from cli.component.loaders import InitLoader
 from cli.constants import (
     COMPONENT_FILE,
     INIT_FILE,
@@ -26,10 +26,10 @@ from cli.constants import (
     SPEC_FILE,
     SPLIGHT_IGNORE,
     TESTS_FILE,
+    TEST_CMD,
 )
 from cli.utils import get_template
 from cli.version import __version__
-
 
 console = Console()
 
@@ -42,7 +42,6 @@ BASE_COMMANDS = {AvailableLanguages.PYTHON: ["python", "main.py"]}
 
 
 class ComponentManager:
-
     _COMPONENT_REQUIRED_FILES = [
         COMPONENT_FILE,
         INIT_FILE,
@@ -55,6 +54,17 @@ class ComponentManager:
     def create(
         self, name: str, version: str = "0.1.0", component_path: str = "."
     ):
+        """Creates the files needed for a new components.
+
+        Parameters
+        ----------
+        name: str
+            The components names
+        version: str
+            The version for the component
+        component_path: str
+            The where to create the files
+        """
         component_name = pascalcase(name)
         absolute_path = os.path.abspath(component_path)
         if not os.path.exists(absolute_path):
@@ -73,6 +83,21 @@ class ComponentManager:
                 f.write(file)
 
     def readme(self, path: str, force: Optional[bool] = False):
+        """Creates a README.md following the information provided in the
+        spec.json file.
+
+        Parameters
+        ----------
+        path: str
+            The path to the component.
+        force: bool
+            Wheter to overwrite an existing readme.
+
+        Raises
+        ------
+        ReadmeExists thrown when the README.md file already exist and
+        force=False
+        """
         spec_file_path = os.path.join(path, SPEC_FILE)
         spec = Spec.from_file(spec_file_path)
         name = spec.name
@@ -101,6 +126,13 @@ class ComponentManager:
         console.print(f"{README_FILE} created for {name} {version}")
 
     def install_requirements(self, path: str):
+        """Installs the requirements of a component
+
+        Parameters
+        -----------
+        path: str
+            The component's path
+        """
         loader = InitLoader(path=path)
         loader.load()
 
@@ -110,6 +142,17 @@ class ComponentManager:
         component_id: str,
         local_environment: bool = False,
     ):
+        """Executes a component
+
+        Parameters
+        ----------
+        path: str
+            The component's path
+        component_id: str
+            The id for the components.
+        local_environment: bool
+            A boolean to define if the component should use local database.
+        """
         spec = Spec.from_file(os.path.join(path, SPEC_FILE))
         self._validate_cli_version(spec.splight_cli_version)
         component_cmd = self._generate_command("python", component_id)
@@ -127,6 +170,41 @@ class ComponentManager:
         if output.returncode != 0:
             raise ComponentExecutionError("Error during component execution")
 
+    def test(
+        self,
+        path: str,
+        name: Optional[str] = None,
+        debug: bool = False,
+    ):
+        todo()
+        # abs_path = str(Path(path).resolve())
+        # if not os.path.exists(abs_path):
+        #     console.print(
+        #         "Error: test file passed as argument does not exists"
+        #     )
+        #     raise ComponentTestFileDoesNotExists(TESTS_FILE)
+        #
+        # # Add path to environ, used in component fixture tests
+        # os.environ["COMPONENT_PATH_FOR_TESTING"] = abs_path
+        # test_path = os.path.join(abs_path, TESTS_FILE)
+        # cmd = " ".join([TEST_CMD, test_path])
+        #
+        # if name:
+        #     cmd = "::".join([cmd, name])
+        #
+        # if debug:
+        #     cmd = " ".join([cmd, "-s"])
+        #
+        # r = subprocess.run(cmd, shell=True, check=True)
+        # stdout, stderr, returncode = r.stdout, r.stderr, r.returncode
+        #
+        # if returncode != 0:
+        #     if stderr:
+        #         console.print(stderr)
+        #     raise ComponentTestError
+        # if stdout:
+        #     console.print(stdout.decode())
+
     def _generate_command(
         self, language: AvailableLanguages, component_id: str
     ) -> List[str]:
@@ -137,38 +215,3 @@ class ComponentManager:
     def _validate_cli_version(self, component_cli_version: str):
         if component_cli_version != __version__:
             raise InvalidSplightCLIVersion(component_cli_version, __version__)
-
-    #
-    # def test(
-    #     self,
-    #     path: str,
-    #     name: Optional[str] = None,
-    #     debug: bool = False,
-    # ):
-    #     abs_path = str(Path(path).resolve())
-    #     if not os.path.exists(abs_path):
-    #         console.print(
-    #             "Error: test file passed as argument does not exists"
-    #         )
-    #         raise ComponentTestFileDoesNotExists(TESTS_FILE)
-    #
-    #     # Add path to environ, used in component fixture tests
-    #     os.environ["COMPONENT_PATH_FOR_TESTING"] = abs_path
-    #     test_path = os.path.join(abs_path, TESTS_FILE)
-    #     cmd = " ".join([TEST_CMD, test_path])
-    #
-    #     if name:
-    #         cmd = "::".join([cmd, name])
-    #
-    #     if debug:
-    #         cmd = " ".join([cmd, "-s"])
-    #
-    #     r = run(cmd, shell=True, check=True)
-    #     stdout, stderr, returncode = r.stdout, r.stderr, r.returncode
-    #
-    #     if returncode != 0:
-    #         if stderr:
-    #             console.print(stderr)
-    #         raise ComponentTestError
-    #     if stdout:
-    #         console.print(stdout.decode())
