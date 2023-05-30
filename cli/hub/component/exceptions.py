2b1ef0116d6b802cb4ec8f19dc2490bdb4257503
marianogaragiola <marianogaragiola@gmail.com>
2023-05-30 20:07:52 -0300

    cleaning

--- cli/hub/component/exceptions.py
+++ cli/hub/component/exceptions.py
@@ -34,7 +34,7 @@ class HubComponentNotFound(Exception):
     """Exception raised when a version is invalid."""
 
     def __init__(self, name: str, version: str):
-        self._msg = f"Hub component {name} version {version} was not found."
+        self._msg = f"Hub component {name} version {version} not found."
 
     def __str__(self) -> str:
         return self._msg
