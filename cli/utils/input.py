628c21adb494a8cdc15333ea8d9db8b878357022
marianogaragiola <marianogaragiola@gmail.com>
2023-06-01 14:31:29 -0300

    move function

--- cli/utils/input.py
+++ cli/utils/input.py
@@ -1,29 +1,66 @@
 from functools import partial
 from pydoc import locate
-from typing import List, Type, Union
+from typing import Dict, List, Type, Union
 
 import click
 
+Primitive = Union[int, str, float, bool]
 
-def list_of(input_value: Union[List, str], param_type: Type = str):
+
+def prompt_param(param: Dict[str, Primitive], prefix: str = "") -> Primitive:
+    """Prompt the user for a single parameter
+
+    Parameters
+    ----------
+    param: Dict
+        The parameter to prompt
+    prefix: str
+        Prefix for the parameter name
+
+    Returns
+    -------
+    Primitive
+    """
+    param_name = param["name"]
+    new_value = input_single(
+        {
+            "name": f"{prefix} {param_name}",
+            "type": param["type"],
+            "required": param.get("required", True),
+            "multiple": param.get("multiple", False),
+            "value": param.get("value"),
+        }
+    )
+    return new_value
+
+
+def list_of(input_value: Union[List, str], param_type: Type = str) -> List:
     if isinstance(input_value, list):
         return input_value
     return [param_type(v.strip()) for v in input_value.split(",")]
 
 
-def input_single(param: dict):
-    """
-    Prompt user for input for a single parameter
-    :param param: Parameter to prompt for
-    :return: Value of parameter
+def input_single(param: Dict[str, Primitive]) -> Primitive:
+    """Prompt user for input for a single parameter
+
+    Parameters
+    ----------
+    param : Dict[str, Primitive]
+
+    Returns
+    -------
+    Primitive the value given by the user
     """
     default = (
         "None"
         if param.get("value") is None and not param["required"]
         else param["value"]
     )
+    required = param.get("required", False)
+    param_name = param.get("name", "")
+    param_type = param.get("type")
     name = (
-        f"{'*' if param['required'] else ' '}{param['name']} ({param['type']})"
+        f"{'*' if required else ' '}{param_name} ({param_type})"
     )
     param_type = locate(param["type"])
     if not param_type:
