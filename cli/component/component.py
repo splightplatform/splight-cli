57dceebd0632a46877d1319d98f9c90db7370720
Agustin Curto <agustin.curto@splight-ai.com>
2023-06-21 19:26:05 +0200

    Move auxiliar functions to utils.py

--- cli/component/component.py
+++ cli/component/component.py
@@ -15,6 +15,12 @@ from cli.component.exceptions import (
     ReadmeExists,
 )
 from cli.component.loaders import InitLoader
+from cli.component.utils import (
+    generate_asset,
+    generate_attribute,
+    generate_component,
+    generate_component_object,
+)
 from cli.constants import (
     INIT_FILE,
     PYTHON_CMD,
@@ -242,65 +248,6 @@ class ComponentManager:
             raise InvalidSplightCLIVersion(component_cli_version, __version__)
 
     def create_local_db(self, path: str):
-        def generate_component(json_spec, component_id):
-            return {
-                component_id: {
-                    "id": component_id,
-                    "name": json_spec.get("name"),
-                    "version": f"{json_spec['name']}-{json_spec['version']}",
-                    "custom_types": json_spec.get("custom_types", []),
-                    "component_type": json_spec.get(
-                        "component_type", "connector"
-                    ),
-                    "input": json_spec.get("input", []),
-                    "output": json_spec.get("output", []),
-                    "commands": json_spec.get("commands", []),
-                    "endpoints": json_spec.get("endpoints", []),
-                    "bindings": json_spec.get("bindings", []),
-                }
-            }
-
-        def generate_component_object(custom_type, component_id):
-            component_object_id = str(uuid4())
-            # TODO: review description and type
-            return {
-                component_object_id: {
-                    "id": component_object_id,
-                    "name": custom_type["name"],
-                    "component_id": component_id,
-                    "description": "",
-                    "type": custom_type["name"],
-                    "data": custom_type.get("fields", []),
-                }
-            }
-
-        def generate_asset(field):
-            asset_id = str(uuid4())
-            # TODO: review attributes
-            return {
-                asset_id: {
-                    "id": asset_id,
-                    "name": field["name"],
-                    "description": field.get("description"),
-                    "tags": [],
-                    "attributes": [],
-                    "verified": False,
-                    "geometry": None,
-                    "centroid": None,
-                    "external_id": None,
-                    "is_public": False,
-                }
-            }
-
-        def generate_attribute(field):
-            attribute_id = str(uuid4())
-            return {
-                attribute_id: {
-                    "id": attribute_id,
-                    "name": field["name"],
-                }
-            }
-
         splight_db = {
             "asset": {},
             "attribute": {},
---
+++ cli/component/utils.py
@@ -0,0 +1,62 @@
+from uuid import uuid4
+
+
+def generate_component(json_spec, component_id):
+    return {
+        component_id: {
+            "id": component_id,
+            "name": json_spec.get("name"),
+            "version": f"{json_spec['name']}-{json_spec['version']}",
+            "custom_types": json_spec.get("custom_types", []),
+            "component_type": json_spec.get("component_type", "connector"),
+            "input": json_spec.get("input", []),
+            "output": json_spec.get("output", []),
+            "commands": json_spec.get("commands", []),
+            "endpoints": json_spec.get("endpoints", []),
+            "bindings": json_spec.get("bindings", []),
+        }
+    }
+
+
+def generate_component_object(custom_type, component_id):
+    component_object_id = str(uuid4())
+    # TODO: review description and type
+    return {
+        component_object_id: {
+            "id": component_object_id,
+            "name": custom_type["name"],
+            "component_id": component_id,
+            "description": "",
+            "type": custom_type["name"],
+            "data": custom_type.get("fields", []),
+        }
+    }
+
+
+def generate_asset(field):
+    asset_id = str(uuid4())
+    # TODO: review attributes
+    return {
+        asset_id: {
+            "id": asset_id,
+            "name": field["name"],
+            "description": field.get("description"),
+            "tags": [],
+            "attributes": [],
+            "verified": False,
+            "geometry": None,
+            "centroid": None,
+            "external_id": None,
+            "is_public": False,
+        }
+    }
+
+
+def generate_attribute(field):
+    attribute_id = str(uuid4())
+    return {
+        attribute_id: {
+            "id": attribute_id,
+            "name": field["name"],
+        }
+    }
