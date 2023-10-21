329d6d4d3c940df29b6d311244c5d0de7e51d240
marianogaragiola <marianogaragiola@gmail.com>
2023-10-21 19:44:01 -0300

    update component template

--- cli/component/templates/main.py
+++ cli/component/templates/main.py
@@ -17,7 +17,7 @@ class {{component_name}}(SplightBaseComponent):
         super().__init__(component_id)
         self._logger = getLogger("MyComponent")
 
-    def start(self):
+    def run_component(self):
         self.execution_engine.start(
             Task(
                 handler=self._run,
