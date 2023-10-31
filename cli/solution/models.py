eb278922357dd4eb363b41147d534e9feb6e0f97
franco <francozacco@gmail.com>
2023-10-31 18:49:56 +0100

    updating models

--- cli/solution/models.py
+++ cli/solution/models.py
@@ -2,19 +2,8 @@ from enum import Enum
 from typing import List, Optional
 
 from pydantic import BaseModel
-from splight_lib.models.base import SplightDatabaseBaseModel
-from splight_lib.models.component import (
-    Asset,
-    Binding,
-    Command,
-    ComponentType,
-    CustomType,
-    Endpoint,
-    InputDataAddress,
-    InputParameter,
-    Output,
-    RoutineStatus,
-)
+from splight_lib.models import Asset, RoutineObject
+from splight_lib.models.component import Component as LibComponent
 
 
 class ElementType(str, Enum):
@@ -22,33 +11,7 @@ class ElementType(str, Enum):
     component = "component"
 
 
-class SplightObject(SplightDatabaseBaseModel):
-    id: Optional[str]
-    name: str
-    component_id: Optional[str]
-    description: Optional[str] = ""
-    type: str
-
-
-class RoutineObject(SplightObject):
-    status: Optional[RoutineStatus] = RoutineStatus.RUNNING
-
-    config: Optional[List[InputParameter]] = []
-    input: List[InputDataAddress] = []
-    output: List[InputDataAddress] = []
-
-
-class Component(SplightDatabaseBaseModel):
-    id: Optional[str]
-    name: Optional[str]
-    version: str
-    custom_types: List[CustomType] = []
-    component_type: ComponentType = ComponentType.CONNECTOR
-    input: List[InputParameter] = []
-    output: List[Output] = []
-    commands: List[Command] = []
-    endpoints: List[Endpoint] = []
-    bindings: List[Binding] = []
+class Component(LibComponent):
     routines: List[RoutineObject] = []
 
 
