567310d1ed53d8a6ece0f5852710169f67fb7706
franco <francozacco@gmail.com>
2023-11-13 11:39:27 +0100

    adds tests and fixes

--- cli/solution/destroyer.py
+++ cli/solution/destroyer.py
@@ -26,6 +26,13 @@ class Destroyer:
             True if it was confirmed and deleted, False instead.
         """
         model_name = model.__name__
+        instance_id = instance_to_delete.id
+        instance_name = instance_to_delete.name
+        if instance_id is None:
+            bprint(
+                f"Cannot destroy {model_name} {instance_name} which has no id."
+            )
+            return False
         bprint(f"You are about to destroy the following {model_name}:")
         bprint(instance_to_delete)
         confirm = confirm_or_yes(self._yes_to_all, "Are you sure?")
