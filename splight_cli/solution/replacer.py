bbf0fcfeab5a772a0ac9141bacd240817a33a49a
smartinellimarco <smartinellimarco@gmail.com>
2023-12-06 09:43:42 -0300

    Finish replacer heuristics

--- splight_cli/solution/replacer.py
+++ splight_cli/solution/replacer.py
@@ -1,3 +1,4 @@
+import json
 from typing import Callable, Dict, List, Optional, Union
 
 from splight_lib.models.component import (
@@ -6,15 +7,10 @@ from splight_lib.models.component import (
     Output,
     RoutineObject,
 )
-from splight_lib.models.function import FunctionItem
 
 from splight_cli.solution.exceptions import UndefinedID
 from splight_cli.solution.models import StateSolution
-from splight_cli.solution.utils import (
-    get_ref_str,
-    is_valid_uuid,
-    parse_str_name,
-)
+from splight_cli.solution.utils import get_ref_str, is_valid_uuid
 
 
 class Replacer:
@@ -61,11 +57,14 @@ class Replacer:
 
         state_functions = self._state.functions
         for i in range(len(state_functions)):
-            function_name = state_functions[i].name
             func_items = state_functions[i].function_items
             for j in range(len(func_items)):
-                self._replace_fn_ref(func_items[j], function_name)
-            # TODO: replace target asset and attribute
+                self._replace_fn_ref(func_item=func_items[j])
+
+            self._replace_fn_ref(target_asset=state_functions[i].target_asset)
+            self._replace_fn_ref(
+                target_attribute=state_functions[i].target_attribute
+            )
 
     def _replace_io_ref(
         self,
@@ -115,31 +114,49 @@ class Replacer:
                     routine.name,
                 )
 
-    def _replace_fn_ref(self, func_item, func_name):
-        # TODO: continue function and check it's working
-        asset = func_item.query_filter_asset
-        # attribute = func_item.query_filter_atttribute
-        if asset is None:  # or attribute is None:
-            return
+    def _replace_fn_ref(
+        self, func_item=None, target_asset=None, target_attribute=None
+    ):
+        if func_item:
+            asset = func_item.query_filter_asset
+            attribute = func_item.query_filter_attribute
+            query_plain = func_item.query_plain
 
-        if asset not in self._reference_map:
-            raise UndefinedID(
-                f"The reference '{asset}' used in the function named "
-                f"'{func_name}' is not a valid reference."
-            )
-        # if attribute not in self._reference_map:
-        #     raise UndefinedID(
-        #         f"The reference '{attribute}' used in the function named "
-        #         f"'{func_name}' is not a valid reference."
-        #     )
+            if asset:
+                value_ref = asset["id"]
+                func_item.query_filter_asset = self._parse_input_output(
+                    value_ref
+                )
 
-        func_item.query_filter_asset = FunctionItem(
-            id=self._reference_map[asset], name=parse_str_name(asset)
-        )
-        # func_item.query_filter_attribute = FunctionItem(
-        #     id=self._reference_map[attribute],
-        #     name=parse_str_name(attribute)
-        # )
+            if attribute:
+                value_ref = attribute["id"]
+                func_item.query_filter_attribute = self._parse_input_output(
+                    value_ref
+                )
+
+            if query_plain:
+                query_dict = json.loads(query_plain)
+
+                # NOTE: sadly this depends on the query.
+                # I'd rather do this than black magic to get the placeholders.
+                asset_ref = query_dict[0]["$match"]["asset"]
+                attribute_ref = query_dict[0]["$match"]["attribute"]
+
+                query_dict[0]["$match"]["asset"] = self._parse_input_output(
+                    asset_ref
+                )
+                query_dict[0]["$match"][
+                    "attribute"
+                ] = self._parse_input_output(attribute_ref)
+                func_item.query_plain = json.dumps(query_dict)
+
+        if target_asset:
+            value_ref = target_asset["id"]
+            target_asset["id"] = self._parse_input_output(value_ref)
+
+        if target_attribute:
+            value_ref = target_attribute["id"]
+            target_attribute["id"] = self._parse_input_output(value_ref)
 
     def _get_new_value(
         self,
@@ -187,7 +204,7 @@ class Replacer:
             return parse_fn(elem.value, component_name, routine_name)
 
     def _parse_input_output(
-        self, value_ref: str, component_name: str, routine_name: str
+        self, value_ref: str, component_name: str = "", routine_name: str = ""
     ) -> str:
         """Parse function for component inputs or outputs.
 
@@ -216,8 +233,7 @@ class Replacer:
             return value_ref
         if value_ref not in self._reference_map:
             raise UndefinedID(
-                f"The reference '{value_ref}' used in the component named "
-                f"'{component_name}' is not a valid reference."
+                f"The reference '{value_ref}' used is not a valid reference."
             )
         return self._reference_map[value_ref]
 
@@ -279,9 +295,6 @@ class Replacer:
             "attribute": self._reference_map[attr_ref],
         }
 
-    def _check_id_is_defined(self, value_ref: str, component_name: str):
+    def _check_id_is_defined(self, value_ref: str):
         if value_ref not in self._reference_map.values():
-            raise UndefinedID(
-                f"The id: {value_ref} used in the component named "
-                f"{component_name} is not defined."
-            )
+            raise UndefinedID(f"The id: {value_ref} is not defined")
