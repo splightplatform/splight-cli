6f6e176bceb1d5a521c580856fd03f4dda888bc5
smartinellimarco <smartinellimarco@gmail.com>
2024-01-19 02:24:58 -0300

    Code checkpoint

--- splight_cli/solution/resources/logger.py
+++ splight_cli/solution/resources/logger.py
@@ -1,95 +1,35 @@
-from rich.console import Console
-from rich.style import Style
+from colorama import Style
 
-console = Console()
-style = Style(bold=True)
+from splight_cli.solution.resources.base import Resource
 
 
-# TODO: deberia estar en el repr esto?
 class ResourceLogger:
-    BASE_MESSAGE = '{type}["{name}"]: %s [id={id}]'
+    template = '{type}["{name}"]: {message} [id={id}]'
 
-    def __init__(
-        self, resource_type: str, resource_name: str, resource_id: str
+    @staticmethod
+    def _bold(message: str) -> str:
+        return f"{Style.BRIGHT}{message}{Style.RESET_ALL}"
+
+    def resource_info(self, message: str, resource: Resource):
+        formatted_message = self.template.format(
+            type=resource.type,
+            name=resource.name,
+            id=resource.id,
+            message=message,
+        )
+        print(self._bold(formatted_message))
+
+    def event(
+        self,
+        message: str,
+        previous_line: bool = False,
+        new_line: bool = False,
+        bold: bool = False,
     ):
-        self.base_msg = self.BASE_MESSAGE.format(
-            type=resource_type,
-            name=resource_name,
-            id=resource_id,
-        )
-
-    def read_start(self):
-        console.print(self.base_msg % "Reading...", style=style)
-
-    def read_complete(self, elapsed_time):
-        console.print(
-            self.base_msg % f"Read complete after {elapsed_time:.2f} seconds",
-            style=style,
-        )
-
-    def refreshing_state(self):
-        console.print(self.base_msg % "Refreshing state...", style=style)
-
-    def diff(self):
-        pass
-
-
-# def diff_json_objects(obj1, obj2):
-#     stack = [("", obj1, obj2)]
-#
-#     while stack:
-#         path, o1, o2 = stack.pop()
-#
-#         for key in o1:
-#             if key not in o2:
-#                 print(f"- {path}.{key} will be removed")
-#             else:
-#                 if isinstance(o1[key], dict) and isinstance(o2[key], dict):
-#                     stack.append((f"{path}.{key}", o1[key], o2[key]))
-#                 elif o1[key] != o2[key]:
-#                     print(f"  # {path}.{key} will be updated")
-#                     print(f"  - {key} = {o1[key]}")
-#                     print(f"  + {key} = {o2[key]}")
-#
-#         for key in o2:
-#             if key not in o1:
-#                 print(f"+ {path}.{key} will be created")
-#
-# # Example usage
-# json_obj1 = {
-#     "module": {
-#         "components": {
-#             "module": {
-#                 "github_resources": {
-#                     "github_repository": {
-#                         "settings": {
-#                             "name": "old_name",
-#                             "allow_merge_commit": True,
-#                             "visibility": "public"
-#                         }
-#                     }
-#                 }
-#             }
-#         }
-#     }
-# }
-#
-# json_obj2 = {
-#     "module": {
-#         "components": {
-#             "module": {
-#                 "github_resources": {
-#                     "github_repository": {
-#                         "settings": {
-#                             "name": "new_name",
-#                             "allow_merge_commit": False,
-#                             "visibility": "private"
-#                         }
-#                     }
-#                 }
-#             }
-#         }
-#     }
-# }
-#
-# diff_json_objects(json_obj1, json_obj2)
+        formatted_message = ""
+        if previous_line:
+            formatted_message += "\n"
+        formatted_message += f"{self._bold(message)}" if bold else message
+        if new_line:
+            formatted_message += "\n"
+        print(formatted_message)
--- splight_cli/solution/resources/manager.py
+++ splight_cli/solution/resources/manager.py
@@ -3,6 +3,7 @@ from typing import List
 from splight_cli.solution.resources.asset import AssetResource
 from splight_cli.solution.resources.base import Resource
 from splight_cli.solution.resources.file import FileResource
+from splight_cli.solution.resources.logger import ResourceLogger
 from splight_cli.solution.state import State
 
 type_map = {
@@ -12,62 +13,109 @@ type_map = {
 
 
 class ResourceManager:
-    def sync(self, state: State):
+    def __init__(self, spec_resources: List[Resource], state: State) -> None:
+        self._state = state
+        self._spec_resources = spec_resources
+        self._to_create = []
+        self._to_update = []
+        self._to_delete = []
+
+        self._logger = ResourceLogger()
+
+    def sync(self, save=False):
         # Sync state resources with respect to the engine
-        for _, type, data in state.all():
+        for _, type, data in self._state.all():
             state_resource = type_map[type](data)
+            self._logger.resource_info("Refreshing state...", state_resource)
             state_resource.sync()
-            state.update(
+            self._state.update(
                 state_resource.name,
                 state_resource.type,
                 state_resource.dump(),
             )
-            state.save()
+            if save:
+                self._state.save()
 
-    def create(self, spec_resources: List[Resource], state: State):
-        # Create or update resources if they are present
-        # in the state or not
-        for spec_resource in spec_resources:
-            # TODO:
-            # If any dependencies:
-            #     Check it is valid (exists in the spec)
-            #     Create those resource/s
-            # Replace references for those IDs (they should be in the state now)
-            if not state.contains(spec_resource.name, spec_resource.type):
-                # Create resource
-                spec_resource.create()
-
-                # Save it to the state
-                state.add(
-                    spec_resource.name,
-                    spec_resource.type,
-                    spec_resource.dump(),
-                )
-                state.save()
+    def plan(self):
+        # TODO: append diffs to a list and print them?
+        for spec_resource in self._spec_resources:
+            if not self._state.contains(
+                spec_resource.name,
+                spec_resource.type,
+            ):
+                self._to_create.append(spec_resource)
             else:
-                # Load resource from the state
-                data = state.get(spec_resource.name, spec_resource.type)
-                state_resource = type_map[spec_resource.type](data)
+                self._to_update.append(spec_resource)
 
-                # Update its parameters with the ones in spec
-                state_resource.update(spec_resource)
-
-                # Update the state data for the resource
-                state.update(
-                    state_resource.name,
-                    state_resource.type,
-                    state_resource.dump(),
-                )
-                state.save()
-
-    def delete(self, spec_resources: List[Resource], state: State):
-        # Delete resources not defined by the spec file
-        for _, type, data in state.all():
+        for _, type, data in self._state.all():
             state_resource = type_map[type](data)
+            if state_resource not in self._spec_resources:
+                self._to_delete.append(state_resource)
 
-            # NOTE: Resources implement '__eq__()'
-            if state_resource not in spec_resources:
-                # TODO: check if any other resource depends on this one?
-                state_resource.delete()
-                state.delete(state_resource.name, state_resource.type)
-                state.save()
+        if not self._to_create + self._to_update + self._to_delete:
+            self._logger.event(
+                "No changes. Your infrastructure matches the configuration.",
+                bold=True,
+                previous_line=True,
+                new_line=True,
+            )
+            self._logger.event(
+                "Splight has compared your real infrastructure against your configuration and found no differences, so no changes are needed.",
+                new_line=True,
+            )
+        else:
+            self._logger.event(
+                "Splight solution will perform the following actions:",
+                previous_line=True,
+                new_line=True,
+            )
+            self._logger.event(
+                f"Plan: {len(self._to_create)} to add, {len(self._to_update)} to change, {len(self._to_delete)} to destroy."
+            )
+
+    def apply(self):
+        # TODO:
+        # Do you want to perform these actions?
+        # Terraform will perform the actions described above.
+        # Only 'yes' will be accepted to approve.
+        #
+        # Enter a value:
+
+        for spec_resource in self._to_create:
+            # Create resource
+            self._logger.resource_info("Creating resource...", spec_resource)
+            spec_resource.create()
+
+            # Save it to the state
+            self._state.add(
+                spec_resource.name,
+                spec_resource.type,
+                spec_resource.dump(),
+            )
+            self._state.save()
+
+        for spec_resource in self._to_update:
+            # Load resource from the state
+            data = self._state.get(spec_resource.name, spec_resource.type)
+            state_resource = type_map[spec_resource.type](data)
+
+            # Update its parameters with the ones in spec
+            self._logger.resource_info("Updating resource...", spec_resource)
+            state_resource.update(spec_resource)
+
+            # Update the state data for the resource
+            self._state.update(
+                state_resource.name,
+                state_resource.type,
+                state_resource.dump(),
+            )
+            self._state.save()
+
+        for state_resource in self._to_delete():
+            # Delete resource from the engine
+            self._logger.resource_info("Deleting resource...", state_resource)
+            state_resource.delete()
+
+            # Delete resource from the state
+            self._state.delete(state_resource.name, state_resource.type)
+            self._state.save()
