f8ef27aded25196d0093fcf8f8e0cae6db14d27a
smartinellimarco <smartinellimarco@gmail.com>
2024-01-31 17:21:08 -0300

    Add some swag

--- splight_cli/solution/resource/diff.py
+++ splight_cli/solution/resource/diff.py
@@ -1,5 +1,7 @@
 from typing import Dict
 
+from colorama import Fore, Style
+
 from splight_cli.solution.dict import get_dict_value, walk_dict
 
 
@@ -13,20 +15,20 @@ def Diff(new_arguments: Dict, old_arguments: Dict) -> Dict:
         try:
             get_dict_value(path, old_arguments)
         except:
-            line = f"\t+ {'.'.join([str(key) for key in path])}: {new_value}"
+            line = f"\t{Fore.GREEN}+{Style.RESET_ALL} {'.'.join([str(key) for key in path])}: {new_value}"
             diff.append(line)
             continue
 
         old_value = get_dict_value(path, old_arguments)
         if new_value != old_value:
-            line = f"\t~ {'.'.join([str(key) for key in path])}: {strike_text(old_value)} -> {new_value} "
+            line = f"\t{Fore.YELLOW}~{Style.RESET_ALL} {'.'.join([str(key) for key in path])}: {strike_text(old_value)} -> {new_value} "
             diff.append(line)
 
     for path, old_value in walk_dict(old_arguments):
         try:
             get_dict_value(path, new_arguments)
         except:
-            line = f"\t- {'.'.join([str(key) for key in path])}: {strike_text(old_value)}"
+            line = f"\t{Fore.RED}-{Style.RESET_ALL} {'.'.join([str(key) for key in path])}: {strike_text(old_value)}"
             diff.append(line)
 
     return diff
