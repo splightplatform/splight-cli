439c75b033682895ac1c724a92f3ca4a26cd254d
smartinellimarco <smartinellimarco@gmail.com>
2024-02-01 14:02:49 -0300

    Add more models

--- splight_cli/solution/resource/models/__init__.py
+++ splight_cli/solution/resource/models/__init__.py
@@ -1,11 +1,15 @@
 from .asset import Asset
+from .attribute import Attribute
 from .base import Resource
 from .file import File
 from .function import Function
+from .metadata import Metadata
 
 __all__ = [
     "Resource",
     "Asset",
+    "Attribute",
+    "Metadata",
     "File",
     "Function",
 ]
