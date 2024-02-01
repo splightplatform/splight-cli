553b1f934389273578efe3a4735740c69f0032c9
smartinellimarco <smartinellimarco@gmail.com>
2024-02-01 15:54:11 -0300

    Implement secrets

--- splight_cli/solution/resource/models/__init__.py
+++ splight_cli/solution/resource/models/__init__.py
@@ -4,10 +4,12 @@ from .base import Resource
 from .file import File
 from .function import Function
 from .metadata import Metadata
+from .secret import Secret
 
 __all__ = [
     "Resource",
     "Asset",
+    "Secret",
     "Attribute",
     "Metadata",
     "File",
---
+++ splight_cli/solution/resource/models/secret.py
@@ -0,0 +1,26 @@
+from splight_lib.models import Secret as SecretSchema
+from splight_lib.models.base import SplightDatabaseBaseModel
+
+from splight_cli.solution.resource.models.base import Resource
+
+
+# FIXME: After the first time this resource is created, the value will be saved
+# unencrypted in the state.
+# This can be fixed easily by overriding the create method.
+# But needs a fix from the splight_lib first, which is, that the model does not
+# reinitialize itself with the API response after the '.save()'.
+# In other words, the model does not contain the encrypted value after '.save()'
+class Secret(Resource):
+    _schema: SplightDatabaseBaseModel = SecretSchema
+
+    def __init__(self, *args, **kwargs):
+        # If this resource already is in the state/engine.
+        if kwargs.get("id", False):
+            arguments = kwargs.get("arguments")
+
+            # Then encrypt the secret value to compare it with the value in the
+            # state.
+            client = self._schema(**arguments)
+            arguments["value"] = client.decrypt(arguments["name"]).value
+
+        super().__init__(*args, **kwargs)
