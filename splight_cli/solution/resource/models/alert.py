111b9b8577c18c97bb4865f60c0fa09f19e14634
smartinellimarco <smartinellimarco@gmail.com>
2024-02-02 15:21:51 -0300

    Add more FIXMEs

--- splight_cli/solution/resource/models/alert.py
+++ splight_cli/solution/resource/models/alert.py
@@ -4,5 +4,11 @@ from splight_lib.models.base import SplightDatabaseBaseModel
 from splight_cli.solution.resource.models.base import Resource
 
 
+# FIXME: Sometimes the API mixes up the order of the alert items after
+# creating the resource, this causes the plan to show a diff.
+# Another apply will fix it, but would be nice if we always sorted new
+# and older arguments by name.
+# Should be done by overriding init and after refresh retrieves the new
+# arguments.
 class Alert(Resource):
     _schema: SplightDatabaseBaseModel = AlertSchema
