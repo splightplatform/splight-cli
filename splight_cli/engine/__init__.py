4fb8612bbca1a2a3e1fdc25802fdf9a06927cbb8
marianogaragiola <marianogaragiola@gmail.com>
2025-08-01 10:45:53 -0300

    minor update

--- splight_cli/engine/__init__.py
+++ splight_cli/engine/__init__.py
@@ -1,12 +1,18 @@
-import typer
+import os
 
-from splight_cli.engine.alert import alert_app
-from splight_cli.engine.asset import asset_app
-from splight_cli.engine.attribute import attribute_app
-from splight_cli.engine.component import component_app
-from splight_cli.engine.datalake import datalake_app
-from splight_cli.engine.file import file_app
-from splight_cli.engine.secret import secret_app
+API_VERSION = os.getenv("API_VERSION", "v3")
+
+import typer  # noqa: E402
+
+if API_VERSION == "v3":
+    from splight_cli.engine.alert import alert_app
+
+from splight_cli.engine.asset import asset_app  # noqa: E402
+from splight_cli.engine.attribute import attribute_app  # noqa: E402
+from splight_cli.engine.component import component_app  # noqa: E402
+from splight_cli.engine.datalake import datalake_app  # noqa: E402
+from splight_cli.engine.file import file_app  # noqa: E402
+from splight_cli.engine.secret import secret_app  # noqa: E402
 
 engine_app = typer.Typer(
     name="Splight Engine",
@@ -15,7 +21,8 @@ engine_app = typer.Typer(
     no_args_is_help=True,
 )
 
-engine_app.add_typer(alert_app, name="alert")
+if API_VERSION == "v3":
+    engine_app.add_typer(alert_app, name="alert")
 engine_app.add_typer(asset_app, name="asset")
 engine_app.add_typer(attribute_app, name="attribute")
 engine_app.add_typer(component_app, name="component")
